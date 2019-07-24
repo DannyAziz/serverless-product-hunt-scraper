f7961b5c6477fa48910b72885191ba0847a9cbb4
Danny Aziz <daanyaal997@gmail.com>
2019-07-24 12:46:07 +0100

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# serverless-product-hunt-scraper
---
+++ app.py
@@ -0,0 +1,28 @@
+from chalice import Chalice
+from requests_html import HTMLSession
+
+app = Chalice(app_name='producthunt-scraper')
+
+
+@app.route("/product-hunt/top-product")
+def get_top_product_product_hunt():
+    session = HTMLSession()
+    url = 'https://www.producthunt.com/'
+    resp = session.get(url)
+
+    product_list = resp.html.find(".postsList_b2208")[1]
+
+    if product_list:
+        top_product = product_list.find("li")[0]
+        product_obj = {
+            "name": top_product.find(".content_31491", first=True).find("h3", first=True).text,
+            "url": "https://producthunt.com{url}".format(url=top_product.find("a", first=True).attrs["href"]),
+            "description": top_product.find(".content_31491", first=True).find("p", first=True).text,
+            "upvote_count": top_product.find(".voteButtonWrap_4c515", first=True).text,
+
+        }
+        return product_obj
+    else:
+        return {"error": "Product List Element Not Found"}
+
+
